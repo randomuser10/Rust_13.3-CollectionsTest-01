b142896d8a85b8529e83b056def317bc6297c171
Mayur Satardekar <mayursatardekar10@gmail.com>
2025-08-20 14:58:48 +0530

    median and mode

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,67 @@
-// to bring HashMaps in current scope
-
+//to bring HashMaps into scope
 use std::collections::HashMap;
 
-//function to check median and mode of a list
-
-fn median_mode(nums: &[i32]) -> Option<f64>{
-    // check if list is empty
+fn median_mode(nums: &[i32]) -> Option<(f64, Vec<i32>)>{
     if nums.is_empty(){
         return None;
     }
     let mut sorted = nums.to_vec();
     //sort the list
+
     sorted.sort_unstable();
-    //save the length of the list
+
+    // get the length of the list
 
     let n = sorted.len();
 
-    let median = if n % 2 == 1  {
-        sorted[n / 2] as f64       
-    } else {
+    let median = if n % 2 == 1{
+        sorted[n / 2] as f64
+    } else{
         let a = sorted[n / 2 - 1] as f64;
         let b = sorted[n / 2] as f64;
-        (a + b) / 2.0
+        ( a + b ) / 2.0
     };
-    Some(median)
 
 
+    // mode
+    // create a hashmap for storing keys and value
+    let mut counts = HashMap::new();
+
+    //fill the hashmap with values
+    for &v in nums{
+        *counts.entry(v).or_insert(0) +=1;
+    }
+
+    // get the highest frequency
+    let max_freq = counts.values().copied().max().unwrap();
+
+    let mut modes: Vec<i32> = counts.into_iter().
+                          filter_map(|(val, freq)| if freq == max_freq { Some(val) } else {None}).
+                          collect();
+    modes.sort_unstable();
+
+    Some((median,modes))
 }
-
 fn main(){
-    let data1 = vec![1,2,3,4,5];
+    let data_1 = vec![3,6,7,8,9,12,9];
+    let data_2: Vec<i32>= vec![];
 
-    match median_mode(&data1){
-        Some(median) => {
-            println!("The median value is {median}");
+    match median_mode(&data_1) {
+        Some((median, modes)) => {
+            println!("The median for the give list is {median:?}");
+            println!("The value of mode is {modes:?}");
         }
-        None => print!("The list is empty!")
+        None => println!("The list is empty")
+        
+    }
+
+    match median_mode(&data_2) {
+        Some((median, modes)) =>{
+            println!("The median value is {median:?}");
+            println!("The mode value is {modes:?}");
+        }
+        None => println!("The list is blank!")
+        
     }
 
 }
